038797894d7621e8a1dae8819abaee3fdfe70dd2
juliusg034 <juliusbermudez03@gmail.com>
2022-08-02 11:08:01 -0700

    adding id attribute corresponding to their number row

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const row3 = document.createElement('div');
 const row4 = document.createElement('div');
 
 row1.classList.add('row');
+row1.setAttribute('id', 'row1')
+
 row2.classList.add('row');
+row2.setAttribute('id', 'row2')
+
 row3.classList.add('row');
+row3.setAttribute('id', 'row3')
+
 row4.classList.add('row');
+row4.setAttribute('id', 'row4')
 
 container.appendChild(row1);
 container.appendChild(row2);
