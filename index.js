c19c7c370d7a21b679dcacac898360a6e31042b3
juliusg034 <juliusbermudez03@gmail.com>
2022-08-02 00:44:09 -0700

    reference container div and other constants

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const container = document.querySelector('#container');
-const row1 = document.createElement('div')
-
-container.appendChild(row1);
+let rows = document.getElementsByClassName('gridRow');
+let cells = document.getElementsByClassName('cell');
