f93e91a27208126dae0f15d1e25d6baefe6707df
juliusg034 <juliusbermudez03@gmail.com>
2022-08-02 11:03:14 -0700

    adding class 'row' to divs in index.js

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const row1 = document.createElement('div');
 const row2 = document.createElement('div');
 const row3 = document.createElement('div');
 const row4 = document.createElement('div');
+
+row1.classList.add('row');
+row2.classList.add('row');
+row3.classList.add('row');
+row4.classList.add('row');
+
+container.appendChild(row1);
+container.appendChild(row2);
+container.appendChild(row3);
+container.appendChild(row4);
