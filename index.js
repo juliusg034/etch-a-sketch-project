4f02ec55b6cc982f1c04859994ab0e5fe4e880d8
juliusg034 <juliusbermudez03@gmail.com>
2022-08-02 00:45:24 -0700

    create defaultGrid function that will create grid

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const container = document.querySelector('#container');
 let rows = document.getElementsByClassName('gridRow');
 let cells = document.getElementsByClassName('cell');
+
+// Creates a deafault grid sized 16x16
+function defaultGrid() {
+    makeRows(16);
+    makeColumsn(16);
+}
+
+// 
