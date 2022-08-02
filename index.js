f1a835022101c575b90bce29bd8b0c1c37efdd93
juliusg034 <juliusbermudez03@gmail.com>
2022-08-02 10:55:40 -0700

    adding four div rows in index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const container = document.querySelector('#container');
+
+const row1 = document.createElement('div');
+const row2 = document.createElement('div');
+const row3 = document.createElement('div');
+const row4 = document.createElement('div');
