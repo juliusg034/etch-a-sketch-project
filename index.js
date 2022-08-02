550297b3476cc8446143f5957c68d8bed11f9946
juliusg034 <juliusbermudez03@gmail.com>
2022-08-02 00:50:15 -0700

    adding functions for defaultGrid function

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ let cells = document.getElementsByClassName('cell');
 // Creates a deafault grid sized 16x16
 function defaultGrid() {
     makeRows(16);
-    makeColumsn(16);
+    makeColumns(16);
 }
 
-// 
+// Takes (rows, columns) input and makes a grid
+function makeRows(rowNums) {
+
+    // Create rows
+    for(let r = 0; r < rowNums; r++){
+        let row = document.createElement('div');
+        container.appendChild(row).className = "grid-row";
+    };
+}
+
+function makeColumns(cellNum) {
+    for(i = 0; i < rows.length; i++) {
+        for(j = 0; j < cellNum; j++) {
+            let newCell = document.createElement('div');
+            row[j].appendChild(newcell).className = 'cell';
+        }
+    }
+}
